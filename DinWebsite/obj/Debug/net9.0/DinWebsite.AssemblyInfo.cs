f53cfdf6ecf40681adc967efc9b01da05c8e0119
John Jairo Porras Vega <jjporras75@gmail.com>
2024-11-25 19:18:52 -0500

    Prueba de las 7:18

--- DinWebsite/obj/Debug/net9.0/DinWebsite.AssemblyInfo.cs
+++ DinWebsite/obj/Debug/net9.0/DinWebsite.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DinWebsite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b1f230d28c53014726acee95c8a056a6422944b")]
 [assembly: System.Reflection.AssemblyProductAttribute("DinWebsite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DinWebsite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
